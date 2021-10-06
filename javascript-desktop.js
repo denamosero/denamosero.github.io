2b1a7a77f70c457648196c34cce1cbd5615bb1a7
denamosero <denamosero@gmail.com>
2021-10-06 13:05:24 +0330

    ...

--- javascript-desktop.js
+++ javascript-desktop.js
@@ -3,7 +3,7 @@
     window.onscroll = function() {myFunction()};
 
     // Get the header
-    var header = document.querySelector(".header-outerCont");
+    var header = document.querySelector(".header-main");
 
     // Get the offset position of the navbar
     var sticky = header.offsetTop;
